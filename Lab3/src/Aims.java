f88b8a3e870d0586be9118f36cb5d46f52d07b5e
Yunasawa <yunasawa100@gmail.com>
2024-11-25 11:01:06 +0700

    Ex7

--- Lab3/src/Aims.java
+++ Lab3/src/Aims.java
@@ -3,29 +3,15 @@ public class Aims
 {
 	public static void main(String[] args) 
 	{
-		Cart anOrder = new Cart();
+		Store store = new Store();
 		
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-		anOrder.addDigitalVideoDisc(dvd1);
-		
 		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-		anOrder.addDigitalVideoDisc(dvd2);
-		
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-		anOrder.addDigitalVideoDisc(dvd3);
-		
-		System.out.printf("Total Cost is: ");
-		System.out.println(anOrder.totalCost());
-		
-		anOrder.removeDigitalVideoDisc(dvd2);
-		
-		System.out.printf("Total Cost is: ");
-		System.out.println(anOrder.totalCost());
-		
-		anOrder.removeDigitalVideoDisc(dvd1);
-		
-		System.out.printf("Total Cost is: ");
-		System.out.println(anOrder.totalCost());
+	
+		store.addDVD(dvd1);
+		store.addDVD(dvd2);
+		store.addDVD(dvd3);
 	}
 
 }

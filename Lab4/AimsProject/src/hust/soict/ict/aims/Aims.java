9f8abea147f66cf9bd638f396e3036a32456d0d7
Yunasawa <yunasawa100@gmail.com>
2024-11-30 16:22:48 +0700

    Ex9

--- Lab4/AimsProject/src/hust/soict/ict/aims/Aims.java
+++ Lab4/AimsProject/src/hust/soict/ict/aims/Aims.java
@@ -11,10 +11,6 @@ public class Aims
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc(0, "The Lion King", "Animation", "Roger Allers", 87, 19.95f);
 		DigitalVideoDisc dvd2 = new DigitalVideoDisc(1, "Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc(2, "Aladin", "Animation", "", 0, 18.99f);
-	
-		store.addDVD(dvd1);
-		store.addDVD(dvd2);
-		store.addDVD(dvd3);
 	}
 
 }

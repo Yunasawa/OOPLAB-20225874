c8ae2591ad081001b4f44bd1e3f7a91652eebad2
Yunasawa <yunasawa100@gmail.com>
2024-11-30 16:04:10 +0700

    Ex8

--- Lab4/AimsProject/src/hust/soict/ict/aims/store/Store.java
+++ Lab4/AimsProject/src/hust/soict/ict/aims/store/Store.java
@@ -1,43 +1,55 @@
 package hust.soict.ict.aims.store;
-import hust.soict.ict.aims.disc.DigitalVideoDisc;
+
+import hust.soict.ict.aims.media.Media;
+import java.util.ArrayList;
 
 public class Store 
 {
-	public static final int MAX_AMOUNT = 100;
-	private DigitalVideoDisc[] _itemsInStore = new DigitalVideoDisc[MAX_AMOUNT];
-	private int _dvdCount = 0;
+    public static final int MAX_AMOUNT = 100;
+    private ArrayList<Media> _itemsInStore = new ArrayList<Media>();
+    private int _mediaCount = 0;
 
-    public void addDVD(DigitalVideoDisc dvd) 
+    public void addMedia(Media media) 
     {
-        if (_dvdCount < _itemsInStore.length) 
+        if (_mediaCount < MAX_AMOUNT) 
         {
-        	_itemsInStore[_dvdCount] = dvd;
-            _dvdCount++;
-            System.out.println("Added: " + dvd);
+            _itemsInStore.add(media);
+            _mediaCount++;
+            System.out.println("Added: " + media);
         } 
-        else System.out.println("Store is full, cannot add DVD.");
-    }
-    
-    public void removeDVD(int dvdId) 
-    {
-        boolean dvdRemoved = false;
-
-        for (int i = 0; i < _dvdCount; i++) 
+        else 
         {
-            if (_itemsInStore[i].getId() == dvdId) 
+            System.out.println("Store is full, cannot add media.");
+        }
+    }
+
+    public void removeMedia(int mediaId) 
+    {
+        boolean mediaRemoved = false;
+        for (int i = 0; i < _mediaCount; i++) 
+        {
+            if (_itemsInStore.get(i).getId() == mediaId) 
             {
-                for (int j = i; j < _dvdCount - 1; j++) 
-                {
-                	_itemsInStore[j] = _itemsInStore[j + 1];
-                }
-                _itemsInStore[_dvdCount - 1] = null; 
-                _dvdCount--;
-                dvdRemoved = true;
-                System.out.println("Removed DVD with ID: " + dvdId);
+                _itemsInStore.remove(i);
+                _mediaCount--;
+                mediaRemoved = true;
+                System.out.println("Removed media with ID: " + mediaId);
                 break;
             }
         }
+        if (!mediaRemoved) 
+        {
+            System.out.println("Media with ID " + mediaId + " not found.");
+        }
+    }
 
-        if (!dvdRemoved) System.out.println("DVD with ID " + dvdId + " not found.");
+    public void printItemsInStore() 
+    {
+        System.out.println("************* ITEMS IN STORE *************");
+        for (int i = 0; i < _mediaCount; i++) 
+        {
+            System.out.println((i + 1) + ". " + _itemsInStore.get(i).toString());
+        }
+        System.out.println("********************************************");
     }
 }

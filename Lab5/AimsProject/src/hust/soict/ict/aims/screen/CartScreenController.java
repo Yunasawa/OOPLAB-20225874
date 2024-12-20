73e5d88c70406edd698997266092cdaf464e15d1
Yunasawa <yunasawa100@gmail.com>
2024-12-20 23:53:18 +0700

    Lab5 - Ex8

--- Lab5/AimsProject/src/hust/soict/ict/aims/screen/CartScreenController.java
+++ Lab5/AimsProject/src/hust/soict/ict/aims/screen/CartScreenController.java
@@ -2,7 +2,11 @@ package hust.soict.ict.aims.screen;
 
 import hust.soict.ict.aims.cart.Cart;
 import hust.soict.ict.aims.media.Media;
+import hust.soict.ict.aims.media.Playable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -11,6 +15,12 @@ public class CartScreenController
 {
     private Cart cart;
 
+    @FXML
+    private Button btnPlay;
+
+    @FXML
+    private Button btnRemove;
+    
     @FXML
     public TableView<Media> tblMedia;
 
@@ -36,5 +46,28 @@ public class CartScreenController
         colMediacategory.setCellValueFactory(new PropertyValueFactory<Media, String>("category"));
         colMediaCost.setCellValueFactory(new PropertyValueFactory<Media, Float>("cost"));
         tblMedia.setItems(this.cart.getItemsOrdered());
+        
+        btnPlay.setVisible(false);
+        btnRemove.setVisible(false);
+
+        tblMedia.getSelectionModel().selectedItemProperty().addListener(
+            new ChangeListener<Media>() 
+            {
+                @Override
+                public void changed(ObservableValue<? extends Media> observable, Media oldValue, Media newValue) 
+                {
+                    if (newValue != null) 
+                    {
+                        updateButtonBar(newValue);
+                    }
+                }
+            });
+    }
+    
+    void updateButtonBar(Media media)
+    {
+    	btnRemove.setVisible(true);
+    	if (media instanceof Playable) btnPlay.setVisible(true);
+    	else btnPlay.setVisible(false);
     }
 }
